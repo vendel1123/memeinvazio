26ee813dc35d5023f3f44913011c21cf61b30f40
tanulo <tanulo@barossgabor.local>
2025-03-17 13:40:36 +0100

    regisztracio html css

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const btnLogin = document.getElementsByClassName("login")[0];
+const btnReg = document.getElementsByClassName("reg")[0];
+
+btnLogin.addEventListener('click', ()=>{
+    window.location.href= '../html/login.html';
+});
+
+
+
+btnReg.addEventListener('click', ()=>{
+    window.location.href= '../html/regisztracio.html';
+});
