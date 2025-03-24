2752df339953424d7b30ed7df629a03022bc919e
tanulo <tanulo@barossgabor.local>
2025-03-24 13:45:32 +0100

    registration function added

---
+++ js/registration.js
@@ -0,0 +1,34 @@
+const btnReg= document.getElementsByClassName('reg')[0]
+
+btnReg.addEventListener('click', register)
+
+async function register(){
+
+    const email = document.getElementById('email').value
+    const name = document.getElementById('name').value
+    const psw = document.getElementById('psw').value
+    const psw2 = document.getElementById('psw2').value
+
+    if(psw !== psw2){
+        return alert('A két jelszó nem egyezik!')
+    }
+
+    try {
+        const response = await fetch('http://127.0.0.1:3000/api/auth/register',{
+            method: 'POST',
+            headers:{
+               'content-type':'application/json'                 
+            },
+            body: JSON.stringify({ email, name, psw })
+        })
+        
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
