f0f50e47aa8c3f6f803cb19bd8aef5cc2fb3a757
tanulo <tanulo@barossgabor.local>
2025-03-31 13:41:07 +0200

    home hmtl létrehozása

--- js/login.js
+++ js/login.js
@@ -1,24 +1,49 @@
-const btnLogin= document.getElementsByClassName("login")[0]
+const btnLogin = document.getElementsByClassName("login")[0]
 
 btnLogin.addEventListener('click', login);
 
-async function login(){
+async function login() {
     const email = document.getElementById('email').value
     const psw = document.getElementById('psw').value
 
     console.log(email, psw);
 
-    const response = await fetch('http://127.0.0.1:3000/api/auth/login'
-        , {
-            method: 'POST',
-            headers:{
-                'content-type':'application/json'
-            },
-            body: JSON.stringify({ email, psw }),
-            credentials: 'include'
-    })
-        
-    
+    try {
+        const response = await fetch('http://127.0.0.1:3000/api/auth/login'
+            , {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ email, psw }),
+                credentials: 'include'
+            })
+        console.log(response);
+        const data = await response.json();
+
+        if (response.ok) {
+            alert(data.message);
+            window.location.href = '../html/home.html';
+        }
+        else if (data.errors) {
+            let errorMessage = '';
+            data.errors.forEach(sor => {
+                errorMessage += `${sor.error}\n`
+            });
+            alert(errorMessage)
+        }
+        else if (data.error) {
+            alert(data.error)
+        }
+        else {
+            alert('Ismeretlen hiba.')
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
 }
 
 
